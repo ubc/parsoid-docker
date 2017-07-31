2f709ab7d59f41f0cabeec8b8eb739f6b130d62d
Pan Luo <pan.luo@ubc.ca>
2017-07-31 01:38:41 -0700

    Add # of worker

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@
 MEDIAWIKI_API_URL="${MEDIAWIKI_API_URL:-http://localhost/w/api.php}"
 MEDIAWIKI_DOMAIN="${MEDIAWIKI_DOMAIN:-localhost}"
 MEDIAWIKI_PREFIX="${MEDIAWIKI_PREFIX:-localhost}"
-#NUM_WORKERS="${NUM_WORKERS:-4}"
+NUM_WORKERS="${NUM_WORKERS:-4}"
 
 cat /usr/local/lib/node_modules/parsoid/config.example.yaml \
     | sed -e "s@uri: 'http://localhost/w/api.php'@uri: '${MEDIAWIKI_API_URL}'@" \
@@ -11,5 +11,6 @@ cat /usr/local/lib/node_modules/parsoid/config.example.yaml \
     | sed -e "s@#metrics:@metrics:@" \
     | sed -e "s@#\(\s*type: log\)@\1@" \
     > /config.yaml
+echo "num_workers: $NUM_WORKERS" >> /config.yaml
 
 exec "$@"
